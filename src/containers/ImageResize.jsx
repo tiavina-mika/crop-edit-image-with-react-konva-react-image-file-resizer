149919a054beb7702daae6b7d0487f0d624015ac
TiavinaMichael <ralainirinatiavina@gmail.com>
2021-10-26 12:00:43 +0300

    clean code

--- src/containers/ImageResize.jsx
+++ src/containers/ImageResize.jsx
@@ -1,38 +1,5 @@
-import React, { useState, useEffect } from "react";
-import Resizer from "react-image-file-resizer";
+import React from "react";
 import { useImageResize } from "../hooks/userImageResize";
-import { convertUrltoFile } from "../utils/utils";
-
-/**
- *
- * @param {*} file
- * @param {number} width
- * @param {number} height
- * @param {string} extension "JPEG, PNG, WEBP"
- * @param {string} outputType "base64, file"
- */
-const resizeFile = ({
-  file,
-  width = 300,
-  height = 300,
-  extension = "JPEG",
-  outputType = "base64"
-}) => {
-  return new Promise((resolve) => {
-    Resizer.imageFileResizer(
-      file,
-      width,
-      height,
-      extension,
-      100,
-      0,
-      (uri) => {
-        resolve(uri);
-      },
-      outputType
-    );
-  });
-};
 
 const ImageResize = ({ file }) => {
   const { resizedImage, initialSize, finalSize } = useImageResize({ file });
